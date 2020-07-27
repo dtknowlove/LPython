ec1ff667b947364d8c0d47534189427f5e56bcb9
dtknowlove <dtknowlove@qq.com>
2020-07-27 10:25:46 +0800

    定时测试通过。

--- Timer/test_schedule.py
+++ Timer/test_schedule.py
@@ -4,26 +4,24 @@ import time
 import json
 import os
 
-# def job():
-#     print("do something")
-
-# schedulecrap.run("10:46",job)
 
 UnityPath="/Applications/Unity2017.4.32.f1/Unity.app" #unity路径
 ProjPath="/Users/admin/Desktop/Unity5.5Projects/PTClassroom/classroom" #工程路径
 Method_Name="UnityModule.TestFun" #方法名
 Method_Para="打开了怎么说？收到没？？" #方法参数;隔开
-cmd='sh ./openunity.sh %s %s %s %s'%(UnityPath,ProjPath,Method_Name,Method_Para)
-proccess=subprocess.Popen(cmd,shell=True,stdout=subprocess.PIPE,stderr=subprocess.PIPE)
+
+def openunity():
+    cmd='sh ./openunity.sh %s %s %s %s'%(UnityPath,ProjPath,Method_Name,Method_Para)
+    return subprocess.Popen(cmd,shell=True,stdout=subprocess.PIPE,stderr=subprocess.PIPE)
 
 # 杀掉Unity进程
 def kill_unity():
-    time.sleep(20)
     processpath_Unity=ProjPath+"/Library/EditorInstance.json";
+    if not os.path.exists(processpath_Unity):
+        return
+    time.sleep(20)
     processid_Unity=-1
-    with open(processpath_Unity,'r') as f:
-        if not os.path.exists(processpath_Unity):
-            return
+    with open(processpath_Unity,'r') as f:        
         data=json.load(f)
         processid_Unity=data['process_id']
         print("current unity processid:",processid_Unity)
@@ -31,12 +29,22 @@ def kill_unity():
         print("kill unity processid:",processid_Unity)
         killprocess= subprocess.Popen('kill -9 %d'%processid_Unity,shell=True,stdout=subprocess.PIPE,stderr=subprocess.PIPE)
         killprocess.wait(5)
+
+
+def job():
+    print("do start...")
+    kill_unity()
+    time.sleep(1)
+    openunity()
+
+schedulecrap.run("10:23",job)
+
 # 测试
-while True:
-    keywords=input('输入要关键字:')
-    if(keywords=="q"):
-        kill_unity()
-        proccess.kill()
-        break
-    else:
-        print("输入q退出")
+# while True:
+#     keywords=input('输入要关键字:')
+#     if(keywords=="q"):
+#         kill_unity()
+#         proccess.kill()
+#         break
+#     else:
+#         print("输入q退出")
